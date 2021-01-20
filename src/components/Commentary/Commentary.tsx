249223fb4144c02ce67e94362489742a14fbfbc7
leo14031999 <34311925+leo14031999@users.noreply.github.com>
2021-01-20 15:08:22 -0500

    se agrego imagenes de comentarios, texto, lineas de separacion

--- src/components/Commentary/Commentary.tsx
+++ src/components/Commentary/Commentary.tsx
@@ -12,30 +12,83 @@ const Usuario = {
 
 }
 const Commentary: FunctionComponent<Props> = ({ }) => {
-    const estrellas = [];
+    const estrellas = {
+        valoracionEstrella: [1,1,1,0,0],
+    };
     return (
         <>
             <h2><div className="text-lg font-bold text-center mb-3 mt-3 p-8">
                 Comentarios
                </div>
             </h2>
-            <div className="md:flex md:auto-cols-max md:grid md:grid-cols-2 max-w-screen-md md:flex md:mx-auto">
+            <div className="grid grid-cols-1 divide-y divide-black-500 ">
+
+            <div className="md:flex md:auto-cols-max md:grid md:grid-cols-2 max-w-screen-md md:flex md:mx-auto md:mx-4 md:p-4 ">
                 <div className="flex">
                     <div
                         className=" md:grid h-8 w-8 md:h-12 md:w-12 bg-cover bg-center md:rounded-md mx-4 p-8"
                         style={{ backgroundImage: `url( ${Usuario.avatarUser} )` }}
                     ></div>
-           
-
                     <div className="md:grid md:grid-rows-2 md:grid-col-2 md:flex md:gap-1 text-justify">
                         <h2 className="font-bold text-xl">{Usuario.nombreUsuario}</h2>
                         <h3 className="text-xs">{Usuario.fechaPublicacion}</h3>
                         <h3 className="text-xs">{Usuario.comentariosUsuario}</h3>
+                        <div className="item-inner-highlight">{estrellas.valoracionEstrella}</div>
 
-                        
                     </div>
                 </div>
             </div>
+        
+            <div className="md:flex md:auto-cols-max md:grid md:grid-cols-2 max-w-screen-md md:flex md:mx-auto md:mx-4 md:p-4">
+                <div className="flex">
+                    <div
+                        className=" md:grid h-8 w-8 md:h-12 md:w-12 bg-cover bg-center md:rounded-md mx-4 p-8"
+                        style={{ backgroundImage: `url( ${Usuario.avatarUser} )` }}
+                    ></div>
+                    <div className="md:grid md:grid-rows-2 md:grid-col-2 md:flex md:gap-1 text-justify">
+                        <h2 className="font-bold text-xl">{Usuario.nombreUsuario}</h2>
+                        <h3 className="text-xs">{Usuario.fechaPublicacion}</h3>
+                        <h3 className="text-xs">{Usuario.comentariosUsuario}</h3>
+                        <div className="item-inner-highlight">......</div>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="md:flex md:auto-cols-max md:grid md:grid-cols-2 max-w-screen-md md:flex md:mx-auto md:mx-4 md:p-4">
+                <div className="flex">
+                    <div
+                        className=" md:grid h-8 w-8 md:h-12 md:w-12 bg-cover bg-center md:rounded-md mx-4 p-8"
+                        style={{ backgroundImage: `url( ${Usuario.avatarUser} )` }}
+                    ></div>
+                    <div className="md:grid md:grid-rows-2 md:grid-col-2 md:flex md:gap-1 text-justify">
+                        <h2 className="font-bold text-xl">{Usuario.nombreUsuario}</h2>
+                        <h3 className="text-xs">{Usuario.fechaPublicacion}</h3>
+                        <h3 className="text-xs">{Usuario.comentariosUsuario}</h3>
+                        <div className="item-inner-highlight">......</div>
+                        
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="md:flex md:auto-cols-max md:grid md:grid-cols-2 max-w-screen-md md:flex md:mx-auto md:mx-4 md:p-4">
+                <div className="flex">
+                    <div
+                        className=" md:grid h-8 w-8 md:h-12 md:w-12 bg-cover bg-center md:rounded-md mx-4 p-8"
+                        style={{ backgroundImage: `url( ${Usuario.avatarUser} )` }}
+                    ></div>
+                    <div className="md:grid md:grid-rows-2 md:grid-col-2 md:flex md:gap-1 text-justify">
+                        <h2 className="font-bold text-xl">{Usuario.nombreUsuario}</h2>
+                        <h3 className="text-xs">{Usuario.fechaPublicacion}</h3>
+                        <h3 className="text-xs">{Usuario.comentariosUsuario}</h3>
+                        <div className="item-inner-highlight">......</div>
+
+                    </div>
+                </div>
+            </div>
+          </div>
         </>
     );
 };
