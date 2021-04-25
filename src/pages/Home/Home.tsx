01e8fe27231fe01fdb823b6b2afff1cec49bc999
Jamt0 <jamt042831@gmail.com>
2021-04-25 00:38:09 -0300

    Se arregla la pantalla negra, pero sigue error de la pantalla blanca por la navegacion

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
-import { IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from "@ionic/react";
-import { searchOutline, navigateOutline, basketOutline, personOutline } from "ionicons/icons";
+import {
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+} from "@ionic/react";
+import {
+  searchOutline,
+  navigateOutline,
+  basketOutline,
+  personOutline,
+} from "ionicons/icons";
 import { FoodBasket } from "pages/FoodBasket/Index";
 import { Perfil } from "pages/Perfil/Index";
 import Explore from "pages/Explore/Explore";
@@ -9,49 +21,46 @@ import Search from "pages/Search/Search";
 import { useSettingsUser } from "context/settingsUser";
 
 const Home: React.FC = () => {
-
   const { textos } = useSettingsUser()!;
 
   console.log("soy tabs");
 
   return (
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/home" exact >
-            <Redirect to="/home/search" />
-          </Route>
-          <Route path="/home/search" exact >
-            <Search />
-          </Route>
-          <Route path="/home/explore" exact >
-            <Explore />
-          </Route>
-          <Route path="/home/foodBasket" exact >
-            <FoodBasket />
-          </Route>
-          <Route path="/home/perfil" exact >
-            <Perfil />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom" className="py-1">
-          <IonTabButton tab="search" href="/home/search">
-            <IonIcon icon={searchOutline} />
-            <IonLabel>{textos["page_buscar"]}</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="explore" href="/home/explore">
-            <IonIcon icon={navigateOutline} />
-            <IonLabel>{textos["page_explorar"]}</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="foodBasket" href="/home/foodBasket">
-            <IonIcon icon={basketOutline} />
-            <IonLabel>{textos["page_almacen"]}</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="perfil" href="/home/perfil">
-            <IonIcon icon={personOutline} />
-            <IonLabel>{textos["page_perfil"]}</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
+    <IonTabs>
+      <IonRouterOutlet>
+        <Route path="/home/search" exact>
+          <Search />
+        </Route>
+        <Route path="/home/explore" exact>
+          <Explore />
+        </Route>
+        <Route path="/home/foodBasket" exact>
+          <FoodBasket />
+        </Route>
+        <Route path="/home/perfil" exact>
+          <Perfil />
+        </Route>
+        <Redirect exact path="/home" to="/home/search" />
+      </IonRouterOutlet>
+      <IonTabBar slot="bottom" className="py-1">
+        <IonTabButton tab="search" href="/home/search">
+          <IonIcon icon={searchOutline} />
+          <IonLabel>{textos["page_buscar"]}</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="explore" href="/home/explore">
+          <IonIcon icon={navigateOutline} />
+          <IonLabel>{textos["page_explorar"]}</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="foodBasket" href="/home/foodBasket">
+          <IonIcon icon={basketOutline} />
+          <IonLabel>{textos["page_almacen"]}</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="perfil" href="/home/perfil">
+          <IonIcon icon={personOutline} />
+          <IonLabel>{textos["page_perfil"]}</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
   );
 };
 
