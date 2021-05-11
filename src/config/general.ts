d6be07926ab12f0f808d032fdaa9884471665657
jamt0 <jamt042831gmail.com>
2021-05-11 10:18:43 -0300

    Package lock modificaciones

--- src/config/general.ts
+++ src/config/general.ts
@@ -1,4 +1,4 @@
-var api_url = process.env.API_URL || "http://192.168.0.158:3000";
+var api_url = process.env.API_URL || "http://localhost:3000";
 console.log('API Host ENV : ' + process.env.API_URL)
 console.log('API Host : ' + api_url)
 
