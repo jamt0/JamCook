58b547db71318fff4c5419d016e8a7165e6c59bd
Jonatan Mancera <alexander.mancera@hasar.com>
2021-06-26 18:37:26 -0300

    Fix style SliderRecipes.

--- src/components/SliderRecipes/index.tsx
+++ src/components/SliderRecipes/index.tsx
@@ -1,67 +1,69 @@
-import { Fragment, FunctionComponent } from "react";
-import { Link } from "react-router-dom";
-import { Text, Slides, Slide, SubTitle, Center, ImageCover } from "UI";
+import { Fragment, FunctionComponent } from 'react';
+import { Link } from 'react-router-dom';
+import { Text, Slides, Slide, SubTitle, Center, ImageCover } from 'UI';
 import { useTranslation } from 'react-i18next';
-import { TRecipe } from "models";
-import namesRoutes from "routes/names";
+import { TRecipe } from 'models';
+import namesRoutes from 'routes/names';
 
 type Props = {
-  slidesPerView: number;
-  listRecipes: {
-    id: string;
-    nameListRecipes: string;
-    recipes: TRecipe[];
-  };
+	slidesPerView: number;
+	listRecipes: {
+		id: string;
+		nameListRecipes: string;
+		recipes: TRecipe[];
+	};
 };
 
 const SliderRecipes: FunctionComponent<Props> = (props) => {
-  const { t } = useTranslation();
+	const { t } = useTranslation();
 
-  const slideOpts = {
-    slidesPerView: props.slidesPerView,
-    spaceBetween: 20,
-  };
+	const slideOpts = {
+		slidesPerView: props.slidesPerView,
+		spaceBetween: 20,
+	};
 
-  return (
-    <Fragment>
-      <div className="grid grid-flow-col gap-8">
-        <SubTitle
-          color="dark"
-          align="left"
-          className="truncate font-black mt-6"
-        >
-          {props.listRecipes.nameListRecipes}
-        </SubTitle>
-        <Center direction="row" justify="end">
-          <Text color="primary" align="right">
-            <Link to={namesRoutes.listRecipes + props.listRecipes.id}>
-              {t('ver_mas')}
-            </Link>
-          </Text>
-        </Center>
-      </div>
-      <Slides options={slideOpts}>
-        {props.listRecipes.recipes.map((recipe, index) => {
-          return (
-            <Slide key={index}>
-              <Link to={namesRoutes.recipe + recipe.id} className="w-full">
-                <div className="flex flex-col">
-                  <ImageCover
-                    src={recipe.pathRecipeImage}
-                    height={44}
-                    rounded
-                  />
-                  <Text color="dark" align="left" className="mt-2 text-base">
-                    {recipe.name}
-                  </Text>
-                </div>
-              </Link>
-            </Slide>
-          );
-        })}
-      </Slides>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<div className='grid grid-flow-col gap-8 mb-3 mt-6'>
+				<Center direction='row' justify='start'>
+					<SubTitle
+						color='dark'
+						align='left'
+						className='truncate font-semibold'
+					>
+						{props.listRecipes.nameListRecipes}
+					</SubTitle>
+				</Center>
+				<Center direction='row' justify='end'>
+					<Text color='primary' align='right'>
+						<Link to={namesRoutes.listRecipes + props.listRecipes.id}>
+							{t('ver_mas')}
+						</Link>
+					</Text>
+				</Center>
+			</div>
+			<Slides options={slideOpts}>
+				{props.listRecipes.recipes.map((recipe, index) => {
+					return (
+						<Slide key={index}>
+							<Link to={namesRoutes.recipe + recipe.id} className='w-full'>
+								<div className='flex flex-col'>
+									<ImageCover
+										src={recipe.pathRecipeImage}
+										height={44}
+										rounded
+									/>
+									<Text color='dark' align='left' className='mt-2 text-base'>
+										{recipe.name}
+									</Text>
+								</div>
+							</Link>
+						</Slide>
+					);
+				})}
+			</Slides>
+		</Fragment>
+	);
 };
 
 export default SliderRecipes;
