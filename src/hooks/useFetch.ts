f1226693cba2d33cc858ec3f06201b585b23b6cb
Jonatan Mancera <alexander.mancera@hasar.com>
2021-06-27 13:28:17 -0300

    Fix state loading in UseFetch.

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -9,7 +9,7 @@ const useFetch = <T>(
 	initialState: T
 ) => {
 	const [error, setError] = useState<string>('');
-	const [loading, setLoading] = useState<boolean>(false);
+	const [loading, setLoading] = useState<boolean>(true);
 	const [data, setData] = useState<T>(initialState);
 
 	const isResponseError = useCallback(
@@ -21,17 +21,18 @@ const useFetch = <T>(
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
+				setLoading(true);
 				const response = await fetchFunction();
 				if (isResponseError(response)) setError(response.error);
 				else setData(response);
+				setLoading(false);
 			} catch (error) {
 				console.log(error);
 				setError('Error de conexión');
+				setLoading(false);
 			}
 		};
-		setLoading(true);
 		fetchData();
-		setLoading(false);
 	}, [fetchFunction, isResponseError]);
 
 	return { error, loading, data };
