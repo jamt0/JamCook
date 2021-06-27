a9afbc0fadb73a18f445755fbcc47b0bc45e23db
Jonatan Mancera <alexander.mancera@hasar.com>
2021-06-27 15:33:27 -0300

    Add Refresher in ScaffoldContent.

--- src/UI/Scaffold/ScaffoldContent/ScaffoldContent.tsx
+++ src/UI/Scaffold/ScaffoldContent/ScaffoldContent.tsx
@@ -1,24 +1,36 @@
-import { Fragment, FunctionComponent } from "react";
-import { IonContent } from "@ionic/react";
+import { Fragment, FunctionComponent } from 'react';
+import { IonContent, IonRefresher, IonRefresherContent } from '@ionic/react';
+import { chevronDownCircleOutline } from 'ionicons/icons';
+import { RefresherEventDetail } from '@ionic/core';
 
 type Props = {
-  fabButton?: React.ReactNode;
-  height?: "auto" | "full" | undefined;
-  margin?: "md" | "lg" | undefined;
-  banner?: React.ReactNode;
+	fabButton?: React.ReactNode;
+	height?: 'auto' | 'full' | undefined;
+	margin?: 'md' | 'lg' | undefined;
+	banner?: React.ReactNode;
+	refresher?: (event: CustomEvent<RefresherEventDetail>) => void;
 };
 
 const ScaffoldContent: FunctionComponent<Props> = ({
-  height = "auto",
-  margin = "lg",
-  ...props
+	height = 'auto',
+	margin = 'lg',
+	...props
 }) => (
-  <IonContent>
-    {props.banner ? props.banner : <Fragment />}
-    <div className={`max-w-screen-${margin} mx-auto p-2 h-${height}`}>
-      {props.children}
-    </div>
-    {props.fabButton ? props.fabButton : <Fragment />}
-  </IonContent>
+	<IonContent>
+		{props.refresher ? (
+			<IonRefresher slot='fixed' onIonRefresh={props.refresher}>
+				<IonRefresherContent
+					pullingIcon={chevronDownCircleOutline}
+				></IonRefresherContent>
+			</IonRefresher>
+		) : (
+			<Fragment />
+		)}
+		{props.banner ?? <Fragment />}
+		<div className={`max-w-screen-${margin} mx-auto p-2 h-${height}`}>
+			{props.children}
+		</div>
+		{props.fabButton ?? <Fragment />}
+	</IonContent>
 );
 export default ScaffoldContent;
